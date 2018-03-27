f477c50a81784608e2517f03c109208e8810ab56
vishal2082 <vishalbhardwaj070@gmail.com>
2018-03-27 14:05:27 +0530

    adding test

--- src/test/java/com/codebind/AppTest.java
+++ src/test/java/com/codebind/AppTest.java
@@ -2,15 +2,13 @@ package com.codebind;
 
 //import static org.junit.Assert.*;
 
-import org.junit.Test;
+//import org.junit.Test;
 
 public class AppTest {
 
-	@Test
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Tests1 is executed ...");
+		System.out.println("Tests1 is execute ...");
 
 }
-	
 }
