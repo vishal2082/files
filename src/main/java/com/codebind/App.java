bb60ea7616fe2e603d9486e36bc5955c9e0c88cf
vishal2082 <vishalbhardwaj070@gmail.com>
2018-03-27 13:03:27 +0530

    first commit

---
+++ src/main/java/com/codebind/App.java
@@ -0,0 +1,10 @@
+package com.codebind;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first maven project");
+	}
+
+}
